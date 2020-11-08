149de2665cd3613d66295f5f9d59a373a7253212
taniseducation <info@tanis.education>
2020-11-08 15:32:38 +0000

    minor

--- public/_mentor/main.php
+++ public/_mentor/main.php
@@ -1,18 +1,19 @@
 <?PHP
-ini_set("display_errors", "1");
 error_reporting(E_ALL);
+
+ini_set("display_errors", "1");
+ini_set('memory_limit','1024M');
+// ini_set('memory_limit', '-1');
+
+// set_time_limit(60);
+// set_time_limit(0);
+ini_set('max_execution_time', 300); //300 seconds = 5 minutes bij 0 zet je hem op onbeperkt
+
 require('class/settings.php');
 require('class/leerling.class');
 require('class/leerlaag.class');
 
-// require('class/vakbeoordeling.class');
-
+// hoofdprogramma
 echo '<p>MAIN loaded</p>';
 require('class/excellezer.php');
-
-
-echo '<PRE>';
-// print_r($vakCodeLijst);
-echo '</PRE>';
-
 ?>
