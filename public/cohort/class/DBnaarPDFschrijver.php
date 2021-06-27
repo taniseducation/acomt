4a99e5ea8519265593d9627aae10ba73a3248f67
taniseducation <info@tanis.education>
2021-06-27 09:11:59 +0000

    eindontwerp PDF

--- public/cohort/class/DBnaarPDFschrijver.php
+++ public/cohort/class/DBnaarPDFschrijver.php
@@ -7,34 +7,13 @@ require_once('ptapdf.class.php');
 $relatief = '/cohort/filePDFout/first.pdf';
 $sFilePath = $_SERVER['DOCUMENT_ROOT'].$relatief ;
 
-// PDF_UNIT can be one of [pt=point, mm=millimeter, cm=centimeter, in=inch]
 $pdf = new PTAPDF('L', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 $pdf->SetCreator('VNR@acomt');
 $pdf->SetAuthor('VNR@acomt');
 $pdf->SetTitle('PTB-PTA-CSG');
 $pdf->SetSubject('Programma van Toetsing en Afsluiting');
 $pdf->SetKeywords('PTO,PTA,CSG,Augustinus,Groningen');
-
-//$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 006', PDF_HEADER_STRING);
-//$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-//$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-//$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
 $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-// $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-// $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-// $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
-
-// font-size issue? https://stackoverflow.com/questions/29795896/tcpdf-how-to-set-font-size-in-right-way
-// $pdf->SetFont('dejavusans', '', 10);
-// $pdf->SetFont('times', '', 10);
-// $pdf->SetFont('helvetica', '', 10);
-
-$pdf->AddPage();
-$pdf->Cell(0, 0, 'breedte: '.$pdf->paginaBreedte, 0, false, 'L', 0, '', 0, false, 'M', 'M');
-
-$pdf->Ln(40);
 
 $data = [
     [1,'4po1',3,'nee',100,'Opdracht bij 3 moderne werken, periode 1940 tot heden. Opdracht bij 3 moderne werken, periode 1940 tot heden.','E','Woordenboek en gelezen werken niet toegestaan'],
@@ -45,19 +24,13 @@ $data = [
     [1,'4po1',3,'nee',100,'Opdracht bij 3 moderne werken, periode 1940 tot heden','E','Woordenboek en gelezen werken niet toegestaan'],
 
 ];
-$pdf->ptaJaarVak($data);
-$pdf->AddPage();
-$pdf->ptaJaarVak($data);
+$laag = '4A';
+$vak='Duitse taal en cultuur';
+$pdf->ptaJaarVak($laag,$vak,$data);
 
 $pdf->lastPage();
+$pdf->Output( $sFilePath , 'F');
 
-// MultiCell(  $w,   $h,   $txt,   $border,   $align = 'J',   $fill = false,   $ln = 1,   $x = '',   $y = '',
-//             $reseth = true,   $stretch,   $ishtml = false,   $autopadding = true,   $maxh,   $valign = 'T',   $fitcell = false) : integer
-/*
-$txt = "Deze tekst is vast te lang, \n hoewel \n de cel nog niet heel groot is. Mijn verwachtingen zijn daarentegen wel groot.";
-$pdf->MultiCell(55, 60, $txt, 1, 'J', 1, 1, 125, 145, true, 0, false, true, 60, 'M', true);
-$pdf->MultiCell(155, 30, $txt, 0, 'C', 1, 2, null, null, true, 0, false, true, 60, 'M', true);
-*/
+
 echo "<h3>$sFilePath path</h3>";
 echo 'dus klik op <a href="'.$relatief.'" target="_NEW">link</a>';
-$pdf->Output( $sFilePath , 'F');
--- public/cohort/class/ptapdf.class.php
+++ public/cohort/class/ptapdf.class.php
@@ -1,19 +1,13 @@
 <?PHP
-// Fijne DOCS // https://rimas-kudelis.github.io/tcpdf/classes/TCPDF.html
-/*  JS settings
-    // A4 formaat is 21 x 29,7 cm | 72 ppi: 595 x 842
-    var margeHorizontaal = 50;
-    var margeVerticaal = 50;
-    var contentBreedte = 842 - 2*margeHorizontaal;
-    var contentHoogte = 595 - 2*margeVerticaal;
-    var grootteKop = 60;
-
-    var PTAType = 'niveau'; // 'vak' of 'niveau'
-    var PTAsoort = 'A'; // A=PTA, B=PTB Bovenbouw
-    var filterVak = 'NE';
-    var filterNiveau = '4A';
-    var voet = true;
-    var waterMerk = false;
+/*  PDF
+    - transparant watermerk
+        https://tcpdf.org/examples/example_042/
+    - autofitting tekst passen in cellen
+        https://tcpdf.org/examples/example_005/
+    - cel documentatie
+        https://rimas-kudelis.github.io/tcpdf/classes/TCPDF.html#method_Cell
+    - multicell documentatie
+        https://rimas-kudelis.github.io/tcpdf/classes/TCPDF.html#method_MultiCell
 */
 class PTAPDF extends TCPDF {
     public function __construct() {
@@ -26,15 +20,18 @@ class PTAPDF extends TCPDF {
         $this->margeVerticaal = 50;
         $this->contentBreedte = 842 - 2*$this->margeHorizontaal;
         $this->contentHoogte = 595 - 2*$this->margeVerticaal;
+        $this->FooterY = -0.5*$this->margeVerticaal;
         $this->grootteKop = 60;
         $this->SetMargins($this->margeHorizontaal,$this->margeVerticaal,null);
         $this->SetHeaderMargin($this->margeVerticaal);
         $this->SetFooterMargin($this->margeVerticaal);
-
-        $this->setHeaderFont(['courier','b',45]);
-        $this->setFooterFont(['helvetica','b',30]); // STAAT NU IN EEN CEL
+        // onderstaande regel eenmalig gebruiken voor genereren
+        // $this->fontName = TCPDF_FONTS::addTTFfont('css/NeueHaas.ttf', 'TrueTypeUnicode', '', 96);
+        // gebruik times helvetica courier dejavusans ++ alles in de map CSS
         $this->SetDefaultMonospacedFont('helvetica');
-        $this->SetFont('helvetica', '', 11); // times helvetica courier dejavusans
+        $this->SetFont('Corbel', '', 11); 
+        $this->setHeaderFont(['NeueHaas',null,36]); // Segoe of NeueHaas
+        $this->setFooterFont(['helvetica','b',30]); // STAAT NU IN EEN CEL
 
         $this->setCellPaddings(5,5,5,5);
         $this->setCellMargins(0,0,0,0);
@@ -47,34 +44,43 @@ class PTAPDF extends TCPDF {
         $this->tabelFillColor = 245;
         $this->tabelLineColor = 240;
         $this->tabelcelHeaders = array('per','SOM','weging','her','duur','stofomschrijving','domeinen examen','hulpmiddelen');
-        $this->defWidth = 50;
-        $this->tabelCelbreedtes = array($this->defWidth,$this->defWidth,$this->defWidth,$this->defWidth,$this->defWidth,170,170,152);
+        $this->defCelheaderhoogte = 20;
+        $this->defCelhoogte = 60;
+        $this->defWidth = 44;
+        $this->tabelCelbreedtes = array($this->defWidth,$this->defWidth,$this->defWidth,$this->defWidth,$this->defWidth,196,196,130);
     }
 
     public function Header() {
         //$image_file = K_PATH_IMAGES.'logo_example.jpg';
         //$this->Image($image_file, 10, 10, 15, '', 'JPG', '', 'T', false, 300, '', false, false, 0, false, false, false);
         // Cell(  $w,   $h,   $txt = '',   $border,   $ln,   $align = '',   $fill = false,   $link = '',
-        //   $stretch,   $ignore_min_height = false,   $calign = 'T',   $valign = 'M') 
-        $this->SetColor($this->HeaderTextColor);
+        //   $stretch,   $ignore_min_height = false,   $calign = 'T',   $valign = 'M')
+        // DIT KAN NIET IN HEADER WANT DIE IS NIET
+        $currentY=$this->GetY();
+        $this->SetTextColor($this->HeaderTextColor);
         $this->SetFillColor($this->HeaderFillColor);
-        $this->Cell($this->contentBreedte,null,'settings: cb='.$this->contentBreedte.' b='.array_sum($this->tabelCelbreedtes), 0, false, 'J', 1, null, 0, false, 'M', 'M');
+        //$this->Cell($this->contentBreedte,null,'settings: cb='.$this->contentBreedte.' b='.array_sum($this->tabelCelbreedtes), 0, false, 'J', 1, null, 0, false, 'M', 'M');
+        $this->Cell($this->contentBreedte,null,'', 0, false, 'C', 1, null, 0, false, 'M', 'M');
+        $this->SetY($currentY - 3);
+        $this->Cell($this->contentBreedte,null,'Kunst - Beeldende vorming', 0, false, 'C', 0, null, 0, false, 'M', 'M');
+        $this->SetFontSize(50);
+        $this->SetY($currentY - 3);
+        $this->Cell($this->defWidth*2,null,'4A', 0, false, 'C', 0, null, 0, false, 'M', 'M');
     }
 
     public function Footer() {
-        $this->SetY(-45); // negatief: vanaf onderen gerekend
-        // SetFont(  $family,   $style = '',   $size = null,   $fontfile = '',   $subset = 'default',   $out = true) 
+        $this->SetY($this->FooterY); // negatief: vanaf onderen gerekend
         $this->SetFont('helvetica',null,10);
         $this->SetTextColor($this->PageNumberColor);
         $this->SetColor(255,0,0);
         $this->Cell($this->contentBreedte,null, '|'.$this->PageNoFormatted().'|', 0, 0,'C',0,null, 0, false, 'M', 'M');
         $this->SetTextColor($this->FooterTextColor);
-        $this->SetY(-45); // negatief: vanaf onderen gerekend
+        $this->SetY($this->FooterY); // negatief: vanaf onderen gerekend
         $this->Cell($this->contentBreedte,null, 'PTA CSG Augustinus', 0, 1,'R',0,null, 0, false, 'M', 'M');
     }
 
 
-    public function ptaJaarVak($data) {
+    public function ptaJaarVak($laag,$vak,$data) {
         // lazy :)
         $w = $this->tabelCelbreedtes;
         $header = $this->tabelcelHeaders;        
@@ -83,10 +89,11 @@ class PTAPDF extends TCPDF {
         $this->SetDrawColor($this->HeaderFillColor);
         $this->SetLineWidth(0.1);
         $this->SetFont('', 'B');
-        $align = 'C';
+        $this->AddPage();
+        $this->Ln(30);
         for($i = 0; $i < count($header); ++$i) {
-            if ($i > 4) {$align = 'L';}
-            $this->MultiCell($w[$i],20,$header[$i],1,$align,true,0,null,null,true,0,false,true,null,'B',false);
+            if ($i > 4) {$align = 'L';} else {$align = 'C';}
+            $this->MultiCell($w[$i],$this->defCelheaderhoogte,$header[$i],1,$align,true,0,null,null,true,0,false,true,null,'B',false);
         }
         $this->Ln();
         $this->SetFillColor($this->tabelFillColor);
@@ -95,14 +102,24 @@ class PTAPDF extends TCPDF {
         $this->SetFont(''); // fontreset
         $fill = 0;
         foreach($data as $row) {
-            $align = 'C';
             for($i = 0; $i < count($header); ++$i) {
-                if ($i > 4) {$align = 'L';}
-                $this->MultiCell($w[$i],70,$row[$i],1,$align,$fill,0,null,null,true,0,false,true,null,'M',true);
+                if ($i > 4) {$align = 'L';} else {$align = 'C';}
+                $this->MultiCell($w[$i],$this->defCelhoogte,$row[$i],1,$align,$fill,0,null,null,true,0,false,true,null,'M',true);
             }
             $this->Ln();
             $fill=!$fill;
         }
+        $currentY=$this->GetY();
+        $this->SetY($currentY + 10);
+        $this->SetFillColor($this->HeaderFillColor);
+        $this->SetTextColor($this->HeaderTextColor);
+        $this->SetDrawColor($this->HeaderFillColor);
+        $this->SetLineWidth(0.1);
+        $this->SetFont('', 'B');        
+        $this->MultiCell($this->contentBreedte,$this->defCelheaderhoogte,'Opmerkingen',1,'L',true,0,null,null,true,0,false,true,null,'B',false);
+        $this->SetTextColor($this->HeaderFillColor);
+        $this->Ln();
+        $this->MultiCell($this->contentBreedte,$this->defCelhoogte,'Dit is opm',1,'L',false,0,null,null,true,0,false,true,null,'B',false);
     }
 }
 ?>
